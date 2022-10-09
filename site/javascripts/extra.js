840ef83434e4acd0121658727f3a4c871a735f42
Stephen Ra <ra.stephen@gene.com>
2022-10-09 02:20:09 -0400

    fix profile

--- site/javascripts/extra.js
+++ site/javascripts/extra.js
@@ -1,2 +1,2 @@
-element = document.getElementsByClassName("md-header__topic")[0];
-element.innerHTML = "Stephen Ra";
+// element = document.getElementsByClassName("md-header__topic")[0];
+// element.innerHTML = "Stephen Ra";
